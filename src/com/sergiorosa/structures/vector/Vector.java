3434a38388cdc1948b747dd3af21cb15f8498ee2
Sergio Rosa <scarlosjazz@gmail.com>
2018-12-21 15:27:33 -0500

    Vector and Arrays: Introduction

---
+++ src/com/sergiorosa/structures/vector/Vector.java
@@ -0,0 +1,10 @@
+package com.sergiorosa.structures.vector;
+
+public class Vector {
+	
+	private String[] elements;
+
+	public Vector(int capacity) {
+		this.elements = new String[capacity];
+	}
+}
---
+++ src/com/sergiorosa/structures/vector/test/Ex_02.java
@@ -0,0 +1,16 @@
+package com.sergiorosa.structures.vector.test;
+
+import com.sergiorosa.structures.vector.Vector;
+
+public class Ex_02 {
+
+	public static void main(String[] args) {
+		
+		Vector vector =  new Vector(5);
+		
+		int i =0;
+		i++;
+
+	}
+
+}
